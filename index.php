15c4d7dcc9a8f28bdc5e6c350fe7be3efac00d2a
BossNzXD <49520939+bossNzXD@users.noreply.github.com>
2021-02-12 17:14:55 +0700

    ละควยไรอะ

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+/=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\
+| @Type | CTF                 |
+| @version | 1.0		      |
+| @Author | BossNzDEV         |
+| @Facebook | Teerawat Luesat |
+\=-=-=-=-=-=-=-=-=-=-=-=-=-=-=/
+*/
+require 'controller/system/router.php';
+
+?>
